b659589e662915c0141499ce54644b1732aa7be5
andrewshumate <shumate@cs.washington.edu>
2021-11-08 18:50:29 -0800

    Get working prototype for checking input and displaying next flag

--- flags/modals.js
+++ flags/modals.js
@@ -1,30 +1,30 @@
-function showErrorModal(correctAnswer) {
-    document.getElementById("results").innerHTML = `No you dumbass, it's <b>${correctAnswer}</b>`;
-
+function showModal() {
     document.getElementById("input").style.display = "none";
     document.getElementById("submit-button").style.display = "none";
 
     document.getElementById("results").style.display = "revert";
     document.getElementById("next-button").style.display = "revert";
 
+    document.getElementById("next-button").focus();
+}
+
+function showErrorModal(correctAnswer) {
+    showModal();
+    document.getElementById("results").innerHTML = `No you dumbass, it's <b>${correctAnswer}</b>`;
     document.getElementById("quiz-section").classList.add("error-animation");
 }
 
 function showSuccessModal() {
+    showModal();
     document.getElementById("results").innerHTML = `Correct!`;
-
-    document.getElementById("input").style.display = "none";
-    document.getElementById("submit-button").style.display = "none";
-
-    document.getElementById("results").style.display = "revert";
-    document.getElementById("next-button").style.display = "revert";
-
     document.getElementById("quiz-section").classList.add("success-animation");
-    document.getElementById("results").innerHTML = `Correct!`;
 }
 
 function hideModal() {
+    document.getElementById("input").value = "";
     document.getElementById("input").style.display = "revert";
+    document.getElementById("input").focus();
+
     document.getElementById("submit-button").style.display = "revert";
 
     document.getElementById("results").style.display = "none";
--- flags/scripts.js
+++ flags/scripts.js
@@ -1,12 +1,43 @@
+let currentCountry;
+
 window.onload = function () {
-    const submitButton = document.getElementById("submit-button");
-    submitButton.addEventListener("click", function() {
-        const input = document.getElementById("input").innerText;
+    // Load up the first county
+    getFlag();
+
+    // Submission listener
+    const onSubmit = function() {
+        const input = document.getElementById("input").value;
         
-        if (Math.random() < 0.5) {
-            showErrorModal("bad");
-        } else {
+        if (input == currentCountry) {
             showSuccessModal();
+        } else {
+            showErrorModal(currentCountry);
         }
+    };
+    const submitButton = document.getElementById("submit-button");
+    submitButton.onclick = onSubmit;
+
+    document.getElementById("input").addEventListener("keyup", function(event) {
+        if (event.key === "Enter") {
+            onSubmit();
+        }
+    });
+
+    // Next button listener
+    const nextButton = document.getElementById("next-button");      
+    nextButton.addEventListener("click", function() {
+        hideModal();
+        getFlag();
     }, false);
 }
+
+function getFlag() {
+    // TODO implement the rest of the logic
+    currentCountry = randomKey(flags);
+    document.getElementById("flag").src = flags[currentCountry];
+}
+
+function randomKey(obj) {
+    var keys = Object.keys(obj);
+    return keys[ keys.length * Math.random() << 0];
+};
