36d613cf9923769dba51b920ced7f2d44fb71fae
andrewshumate <shumate@cs.washington.edu>
2021-11-08 17:47:36 -0800

    Add skeleton for correct answer

--- flags/modals.js
+++ flags/modals.js
@@ -1,4 +1,6 @@
-function showErrorModal() {
+function showErrorModal(correctAnswer) {
+    document.getElementById("results").innerHTML = `No you dumbass, it's <b>${correctAnswer}</b>`;
+
     document.getElementById("input").style.display = "none";
     document.getElementById("submit-button").style.display = "none";
 
@@ -8,7 +10,20 @@ function showErrorModal() {
     document.getElementById("quiz-section").classList.add("error-animation");
 }
 
-function hideErrorModal() {
+function showSuccessModal() {
+    document.getElementById("results").innerHTML = `Correct!`;
+
+    document.getElementById("input").style.display = "none";
+    document.getElementById("submit-button").style.display = "none";
+
+    document.getElementById("results").style.display = "revert";
+    document.getElementById("next-button").style.display = "revert";
+
+    document.getElementById("quiz-section").classList.add("success-animation");
+    document.getElementById("results").innerHTML = `Correct!`;
+}
+
+function hideModal() {
     document.getElementById("input").style.display = "revert";
     document.getElementById("submit-button").style.display = "revert";
 
@@ -16,4 +31,5 @@ function hideErrorModal() {
     document.getElementById("next-button").style.display = "none";
 
     document.getElementById("quiz-section").classList.remove("error-animation");
+    document.getElementById("quiz-section").classList.remove("success-animation");
 }
--- flags/scripts.js
+++ flags/scripts.js
@@ -1,3 +1,12 @@
 window.onload = function () {
-
+    const submitButton = document.getElementById("submit-button");
+    submitButton.addEventListener("click", function() {
+        const input = document.getElementById("input").innerText;
+        
+        if (Math.random() < 0.5) {
+            showErrorModal("bad");
+        } else {
+            showSuccessModal();
+        }
+    }, false);
 }
